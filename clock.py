92f20656e9b60b1b7bb505c124a52bf1c0536813
Raymond Oeta <oetaray@gmail.com>
2023-08-20 20:37:52 +0300

    first commit

---
+++ clock.py
@@ -0,0 +1,20 @@
+def time_converter(hour, minute, period):
+    #Converting hour to 24hour format
+    if period == 'am':
+        if hour == 12:
+            hour = 0
+        else:
+            if hour != 12:
+                hour += 12
+    # Convert hour and minute to string with leading zeros
+    hour_str = str(hour).zfill(2)
+    minute_str = str(minute).zfill(2)
+    
+    #Combining hour and minute strings
+    time_string = hour_str + ':' + minute_str
+    return time_string
+
+#Testing the function...
+print(time_converter(12, 10, 'am'))
+print(time_converter(1, 25, 'pm'))
+print(time_converter(5, 12, 'am'))
